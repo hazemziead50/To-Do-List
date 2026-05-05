8bc6c63d0779164cff978885591ad4be60c2347c
Hazem Ziead <hazemziead50@gmail.com>
2026-05-05 12:19:53 +0300

    v.4

--- script.js
+++ script.js
@@ -2,95 +2,125 @@ let addButton = document.getElementById('add-btn');
 let taskInput = document.getElementById('taskInput');  
 let taskContainer = document.querySelector('.todo-container'); 
 
-    document.addEventListener('DOMContentLoaded', loadTasks) 
-    
-
-addButton.addEventListener('click', e => {
-    let task = taskInput.value.trim()
-    if(task !== ''){
-        const id = Math.random()
-        
-        let taskElement = createTaskElement(id,task)
-        taskContainer.appendChild(taskElement)
-        saveTasks({id,task,isChecked:false})
-        taskInput.value = ''
-    } else {
-        taskInput.placeholder = "الرجاء إدخال مهمة أولاً";
-    } 
-})
-
-function loadTasks (){
-    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || []
-    const taskElements = storedTasks.map(task => createTaskElement(task.id,task.task,task.isChecked))
-    taskContainer.append(...taskElements)
-}
-
-function createTaskElement(id,taskText, isChecked = false) {
-    let taskDiv = document.createElement('div')
-    taskDiv.setAttribute('id',id)
-    let inputCheckBox = document.createElement('input');
-    let deleteBtn = document.createElement('button');
-    let li = document.createElement('li');
+    class Task{
+        constructor({id,taskText,isChecked}){
+            this.id = id;
+            this.taskText = taskText;
+            this.isChecked = isChecked;
+        }
+        create(){
+        let taskDiv = document.createElement('div')
+        taskDiv.setAttribute('id',this.id)
+        let inputCheckBox = document.createElement('input');
+        let deleteBtn = document.createElement('button');
+        let li = document.createElement('li');
         taskDiv.classList.add('task')
         inputCheckBox.type = "checkbox"
-        inputCheckBox.checked = isChecked
-        if(isChecked) {
+        inputCheckBox.checked = this.isChecked
+        if(this.isChecked) {
             li.style.textDecoration = 'line-through'
         }
-        li.textContent = taskText;
+        li.textContent = this.taskText;
         deleteBtn.textContent = 'Delete';
         deleteBtn.classList.add('delete-btn')
-        deleteBtn.dataset.id = id
+        deleteBtn.dataset.id = this.id
         taskDiv.append(li,inputCheckBox,deleteBtn)
-        
         return taskDiv
-        
-}
-
-
- // Delete task
-taskContainer.addEventListener('click', e => {
-    if (e.target.classList.contains('delete-btn')) {
-        const taskId = e.target.getAttribute('data-id')
-        const taskElement = document.getElementById(taskId)
-        taskElement.remove()
-        removeFromStorage(taskId);
-    }
-})
-//mark copmlete
-taskContainer.addEventListener('click', e => {
-    if (e.target.type === 'checkbox') {
-        const taskId = e.target.parentElement.getAttribute('id')
-        const li = e.target.parentElement.querySelector('li')
-        if (e.target.checked) {
-            li.style.textDecoration = 'line-through'
-            updateTaskStatus(taskId, true)
-        }else{
-            li.style.textDecoration = 'none'
-            updateTaskStatus(taskId, false)
         }
     }
-})
-
-function removeFromStorage(taskId) {
-    let parsedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    const filterdTasks = parsedTasks.filter(task => task.id != taskId);
-    localStorage.setItem('tasks', JSON.stringify(filterdTasks));
-}
-function updateTaskStatus(taskId, isChecked){
-    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || []
-    const updatedTasks = storedTasks.map(task =>{
-        if(task.id == taskId){
-            task.isChecked = isChecked
+    class TaskList{
+        #container
+        constructor(selector){
+            this.#container = document.querySelector(selector)
         }
-        return task
+        append(...taskElement){
+            this.#container.append(...taskElement)
+        }
+
+    }
+    class Storage{
+        #key
+        constructor(key){
+            this.#key = key
+        }
+        getList(){
+            return JSON.parse(localStorage.getItem(this.#key)) || []
+        }
+        save(item){
+            const parsed = JSON.parse(localStorage.getItem(this.#key)) || []
+            parsed.push(item)
+            localStorage.setItem(this.#key,JSON.stringify(parsed))
+        }
+        updateTaskStatus(taskID,isChecked){
+            const list = storage.getList()
+            const newList = list.map(task => {
+                if(task.id == taskID){
+                    task.isChecked = isChecked
+                } 
+                return task
+            })
+            localStorage.setItem('tasks',JSON.stringify(newList))
+        }
+        upDateDelete(taskId){
+            const parsed = storage.getList()
+            const newList = parsed.filter(task => task.id != taskId)
+            localStorage.setItem('tasks',JSON.stringify(newList))
+        }
+    }
+
+    const taskList = new TaskList('.todo-container')
+    const storage = new Storage('tasks')
+
+
+    document.addEventListener('DOMContentLoaded',(e) => {
+
+        const list = storage.getList()
+        const taskElements =list.map(item => {
+            const task = new Task(item)
+            return task.create()
+        })
+        taskList.append(...taskElements)
     })
-    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-}
-function saveTasks(taskElement){
-    const parsedTasks = JSON.parse(localStorage.getItem('tasks')) || []
-    parsedTasks.push(taskElement)
-    localStorage.setItem('tasks',JSON.stringify(parsedTasks))
-    }
+
+    addButton.addEventListener('click', e => {
+        const taskText = taskInput.value.trim()
+        if(taskText !== ''){
+            const id = Math.random()
+            const task = new Task({id, taskText, isChecked: false})
+            const taskElement = task.create()
+            taskList.append(taskElement)
+            storage.save({id, taskText, isChecked: false})
+            taskInput.value = ''
+        } else {
+            taskInput.placeholder = "الرجاء إدخال مهمة أولاً";
+        }
+    })
+
+    taskContainer.addEventListener('click', e => {
+        if(e.target.classList.contains('delete-btn')){
+            const taskId = e.target.getAttribute('data-id')
+            const taskElement = document.getElementById(taskId)
+            taskElement.remove()
+            console.log(taskId);
+            
+            storage.upDateDelete(taskId)
+        }
+    })
+    taskContainer.addEventListener('click', e =>{
+        if(e.target.type === 'checkbox'){
+            const taskId = e.target.parentElement.getAttribute('id') 
+            const li = e.target.parentElement.querySelector('li')
+
+            if(e.target.checked){
+                li.style.textDecoration = 'line-through'
+                storage.updateTaskStatus(taskId, true)
+            }else {
+                li.style.textDecoration = 'none'
+                storage.updateTaskStatus(taskId, false)
+            }
+        
+            
+        }
+    })
 
 
